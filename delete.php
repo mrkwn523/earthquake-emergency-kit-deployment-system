d421398c002801ebae04c4ff87e9408e00a0d68a
KeiranJamesCatalan <catalanka@students.nu-fairview.edu.ph>
2026-02-07 21:38:21 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,12 +1,15 @@
 <?php
 include("db_connect.php");
 
+
 if(isset($_GET['id'])) {
     $id = (int)$_GET['id']; 
+
 
     $conn->query("DELETE FROM kits WHERE id = $id");
 }
 
-header("Location: index.php");
+
+header("Location: index.php?deleted=1");
 exit();
 ?>
