268921f2a613a0174474818b7ec6ae350f702015
mrkwn523 <johnmarkwendalde@gmail.com>
2026-02-08 23:17:15 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,13 +1,12 @@
 CREATE TABLE kits (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  kit_name VARCHAR(100) NOT NULL,
-  location VARCHAR(100),
-  status ENUM('Available','Deployed','Needs Restock') DEFAULT 'Available',
+  kit_type VARCHAR(100) NOT NULL,
+  location VARCHAR(255),
+  status ENUM('Currently Restocked','Needs Restock') DEFAULT 'Currently Restocked',
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 INSERT INTO kits (kit_name, location, status) VALUES
-('Family Kit', 'Caloocan City', 'Available'),
-('First Aid Kit', 'Manila', 'Needs Restock'),
-('Rescue Kit', 'Quezon City', 'Deployed');
+('Family Kit', 'Caloocan City', 'Currently Restocked'),
+('First Aid Kit', 'Manila', 'Needs Restock');
