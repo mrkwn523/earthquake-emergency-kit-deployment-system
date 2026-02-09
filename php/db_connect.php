dfd47b4a455aa4ee1b3f333de20221c80b968e07
KeiranJamesCatalan <catalanka@students.nu-fairview.edu.ph>
2026-02-09 09:07:39 +0800

    Update db_connect.php

--- php/db_connect.php
+++ php/db_connect.php
@@ -4,8 +4,10 @@ $username   = "if0_41088693";
 $password   = "m8s2xKqc4LiD7TE";        
 $dbname     = "if0_41088693_db_earthquakekit";           
 
+// Create a connection to the database
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check if the connection was successful
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
